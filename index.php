b2bc0ed01f5421e4c96446abbf9e576d9d870cc3
lulu1417 <boa1417@gmail.com>
2020-03-17 14:56:49 +0800

    feat:view:index

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+require 'core/Router.php';
+
+$router = new Router;
+
+require 'routes.php';
+
+$router->define([
+    '' => 'controllers/index.php',
+    'board' => 'controllers/board.php',
+    'storePost' => 'controllers/storePost.php',
+    'storeComment' => 'controllers/storeComment.php',
+    'showComments' => 'controllers/showComments.php',
+    'storeReply' => 'controllers/storeReply.php',
+    'showReplies' => 'controllers/showReplies.php',
+    'storeLike' => 'controllers/storeLike.php',
+    'showLikes' => 'controllers/showLikes.php',
+]);
+
+$uri = trim($_SERVER['REQUEST_URI'],'/');
+
+require $router->direct($uri); //this will return uri and its corresponding file
--- routes.php
+++ routes.php
@@ -1,1 +1,12 @@
 <?php
+$router->define([
+    '' => 'controllers/index.php',
+    'board' => 'controllers/board.php',
+    'storePost' => 'controllers/storePost.php',
+    'storeComment' => 'controllers/storeComment.php',
+    'showComments' => 'controllers/showComments.php',
+    'storeReply' => 'controllers/storeReply.php',
+    'showReplies' => 'controllers/showReplies.php',
+    'storeLike' => 'controllers/storeLike.php',
+    'showLikes' => 'controllers/showLikes.php',
+]);
